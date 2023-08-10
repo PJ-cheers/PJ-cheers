9e19efa5273e7218fc9058bbf254e75f5fe76ee2
김채현 <kimchennie@naver.com>
2023-08-10 16:14:19 +0900

    feat/diy칵테일 페이지 ui

--- src/api/recipeData.js
+++ src/api/recipeData.js
@@ -1,15 +1,6 @@
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../firebase';
 
-const getFirestoreData = async () => {
-  const querySnapshot = await getDocs(collection(db, 'recipes'));
-  const fetchedData = querySnapshot.docs.map((doc) => ({
-    ...doc.data(),
-    id: doc.id
-  }));
-  return fetchedData;
-};
-
 const getDIYData = async () => {
   const querySnapshot = await getDocs(collection(db, 'DIY'));
   const fetchedData = querySnapshot.docs.map((doc) => ({
@@ -19,4 +10,4 @@ const getDIYData = async () => {
   return fetchedData;
 };
 
-export { getFirestoreData, getDIYData };
+export { getDIYData };
